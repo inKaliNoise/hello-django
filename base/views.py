a39fbd48e9623eccc01fc56697ee027230116801
noise <kooonrad.noise@gmail.com>
2023-05-16 21:11:40 +0200

    init steps in django

---
+++ base/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+
+rooms = [
+    {'id': 1, 'name': 'Lets learn python!'},
+    {'id': 2, 'name': 'Design with me!'},
+    {'id': 2, 'name': 'Frontend developers!'},
+
+]
+
+ 
+def home(request):
+    return render(request, 'base/home.html', {'rooms':rooms})
+
+
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
